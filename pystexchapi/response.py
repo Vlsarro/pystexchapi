3b1d42761c7326ddd86a79a23d79f9f636214514
Vladislav <drekmaro@gmail.com>
2018-07-04 09:36:32 +0300

    Add error checking method prototype

--- pystexchapi/response.py
+++ pystexchapi/response.py
@@ -19,4 +19,9 @@ class StockExchangeResponseParser(object):
         except ValueError as e:
             raise APIResponseParsingException(exc=e, response=response)
         else:
+            cls.check_for_errors(data)
             return data
+
+    @staticmethod
+    def check_for_errors(data: dict):
+        pass  # TODO: implement error checking according to API format
